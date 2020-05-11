300aff0cd2b5cca14f61ccc040c77277a8be4ce3
Raquel Andrade <raquelbennington@gmail.com>
2020-05-11 12:26:28 -0300

    atualização 3

---
+++ Aula 1 - Conceitos/Exemplos/3_attribute/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+$('p').html(`Olá é igual a 
+<span class="cor">Hello</span> 
+<strong>Darkness</strong>
+`);
+
+$('li').addClass("margin")
+
+$('li').removeClass("item1")
+
+$('p').click(function() {
+    $(this).toggleClass("destaque")
+    
+})
+
+
+
+})
