2938867481deb9c9a0a39838689913ad3d1a7844
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2020-05-13 00:27:19 -0300

    Atualização com a Aula 3

---
+++ Aula 3 - Animações/Exercicios/01_scroll/index.js
@@ -0,0 +1,12 @@
+// BY KAREN GRIGORYAN
+
+/*
+    1. Animar os elementos do menu lateral de modo que quando cada um dos links for clicado,
+    o scroll acontecerá de forma lenta, direcionado o scroll para o heading correspondente 
+        ex. se eu clicar no link 3, o scroll deve me direcionar para o heading 3
+
+    2. Criar o botão de scroll até o topo da página. Lembrando que esse botão só deve aparecer 
+    a partir do momento que o scroll começou a ir para baixo. 
+
+
+*/
---
+++ Aula 3 - Animações/README.md
@@ -0,0 +1,9 @@
+EVENTS: https://api.jquery.com/category/events/
+
+1. Eventos
+   - Eventos de navegador
+   - Eventos de Mouse
+   - Eventos de Teclado
+2. Show, ride, toggle
+3. FadeIn, FadeOut
+4. Error
