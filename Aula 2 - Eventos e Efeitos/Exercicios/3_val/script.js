1b33624094de0b38d35afaba2fda6f855867ff93
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2020-05-15 19:30:19 -0300

    resolução dos exercicios 3 e 4 das aulas 2 e 3

--- Aula 2 - Eventos e Efeitos/Exercicios/3_val/script.js
+++ Aula 2 - Eventos e Efeitos/Exercicios/3_val/script.js
@@ -10,3 +10,23 @@ do input deve completar a frase.
 
 
 */
+
+$(document).ready(function () {
+  $("#botao-replicar").click(function () {
+      //'guardar' meus elementos em variáveis
+    let input = $("#campo-nome");
+    //tratar o input, pegando apenas o seu valor e tratando os espaços em branco
+    //utilizando o método trim()
+    let nome = input.val().trim();
+
+    //validação: nome ! = "" => "se não houver input de valor vazio"
+    if (nome != "") {
+    //'limpa' o que estiver escrito no input, 
+    // e agora seu valor é igual a vazio ("")
+      input.val("");
+      +$("#texto-replicado").text(
+        "A desenvolvedora mais incrível desse ano será a " + nome
+      );
+    }
+  });
+});
--- Aula 2 - Eventos e Efeitos/Exercicios/4_nav/script.js
+++ Aula 2 - Eventos e Efeitos/Exercicios/4_nav/script.js
@@ -1,2 +1,21 @@
 //1. Faça com que os elementos dentro do menu aparecem quando o mouse passar por eles.
 //2. Os elementos devem desaparecer quando o mouse não estiver passando por eles
+
+$(document).ready(function(){
+    //1ª resolução - utilizando o evento de Hover
+    $('li').hover(function(){
+        //this => qualquer elemento li que foi clicado. 
+        //find() => método que busca dentro do elemento selecionado a classe indicada
+        $(this).find('.menu2').slideDown();
+    },
+    function(){
+        $(this).find('.menu2').slideUp();
+    });
+
+    //1ª resolução - Utilizando o evento de Click
+    $('li').click(function(){
+        $(this).find('.menu2').slideToggle();
+        //siblings => os parentes do elemento li clicado
+        $(this).siblings().find('.menu2').slideUp();
+    })
+});
