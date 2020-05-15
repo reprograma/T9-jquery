f1483d06439a6ad1c04a1fca811801a5b855f8b0
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2020-05-15 19:33:59 -0300

    adição de comentários nos exercicios 1 e 2 da aula 2

--- Aula 2 - Eventos e Efeitos/Exercicios/1_class/script.js
+++ Aula 2 - Eventos e Efeitos/Exercicios/1_class/script.js
@@ -7,35 +7,46 @@
   OK 3. Imagine que você muda com frequencia de opinião sobre um dos seus filmes favoritos. 
   Sendo assim, crie uma variável "favDoMomento" com esse filme
 
-  OK 3. selecione o h1 e altere seu texto para "sobre mim". Acrescente também a classe 'titulo' para que ele 
+  OK 4. selecione o h1 e altere seu texto para "sobre mim". Acrescente também a classe 'titulo' para que ele 
   fique bacanudo
 
-  OK 4. Adicione as classes 'list' e 'item-list' na lista e nos itens de lista, utilizando JQuery
+  OK 5. Adicione as classes 'list' e 'item-list' na lista e nos itens de lista, utilizando JQuery
   
-  OK 5. Adicionei uma classe errada! Suma com a classe 'wrongClass', utilizando JQuery
+  OK 6. Adicionei uma classe errada! Suma com a classe 'wrongClass', utilizando JQuery
 */
 
 $(document).ready(function() {
   
+  //2.
   let nickname = "Bru"
   let cidade = "São Paulo"
   let favoritos = "Os Miseráveis, Mulan"
+  //3.
   let favDoMomento = "Parasita"
 
+  //1.
   $("body").css("font-family", "Arial, sans-serif")
 
+  //2.
   $("#nickname").text(nickname)
   $("#cidade").text(cidade)
+  //o método text() pode receber mais de um argumento/parâmetro
   $("#favoritos").text([favoritos, favDoMomento])
 
   $("h1")
     .text("Sobre mim")
     .addClass("titulo")
 
+
+  //addClass() adiciona uma classe CSS no elemento selecionado. O nome da 
+  //classe é passado como parâmetro
   $("li").addClass("list")
   $("span").addClass("item-list")
 
   //resolução 1
+
+  //removeClass() adiciona uma classe CSS no elemento selecionado. O nome da 
+  //classe é passado como parâmetro
   $("li").removeClass("wrongClass")
 
   //resolução 2
--- Aula 2 - Eventos e Efeitos/Exercicios/2_menu/script.js
+++ Aula 2 - Eventos e Efeitos/Exercicios/2_menu/script.js
@@ -2,11 +2,13 @@
 // 2. Faça com que o menu lateral desapareça
 
 
-//1ª resolução
+//1ª resolução - Utilizando apenas modificações no CSS
 
 $(document).ready(function(){
     $("#botaoAbrir").click(function() {
         $("#menulateral").css("display", "block");
+        //"afasta" visualmente os elementos da div para 
+        //longe do menu
         $(".div-botao").css("margin-left", "250px")
     })
     $("#botaoFechar").click(function() {
@@ -15,7 +17,7 @@ $(document).ready(function(){
     })
 })
 
-//2ª resolução
+//2ª resolução - Utilizando efeitos de aparecer/desaparecer
 $("#botaoAbrir").click(function(){
     $("#menulateral").toggle()
     $(".div-botao").css("margin-left", "250px")
