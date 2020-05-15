f687ef386483e2fe49f399aa5c14a36ca9dd9a68
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2020-05-15 18:25:20 -0300

    Exemplo de scroll 'down' e resolução do exercício de scroll

--- Aula 3 - Animações/Exemplos/02_scroll/2_scrolldown/script.js
+++ Aula 3 - Animações/Exemplos/02_scroll/2_scrolldown/script.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
-    console.log($("#button").offset(), 'quem é o offset')
+    
+    //exemplo de offset
+    $("#button").click(function() {
+        console.log($("#button").offset(), 'offset')
+       console.log($("#button").position(), 'position left')
+   })
 
-   $("#button").click(function() {
-       alert("Left:" + $(this).offset().left)
+   //"descer" => referencia: andar do elevador
+   $(".down").click(function() {
+       $("html, body").animate({
+           scrollTop: $(".titulo").offset().top
+       },
+       5000
+       );
+   });
+
+   //"subir" => retornando as origens
+   $(".up").click(function() {
+       $("html, body").animate({
+           scrollTop: "0"
+       },
+       5000
+       );
+       console.log($(".titulo").offset().top, 'top do meu botao up')
    })
 })
--- Aula 3 - Animações/Exercicios/01_scroll/index.js
+++ Aula 3 - Animações/Exercicios/01_scroll/index.js
@@ -8,5 +8,64 @@
     2. Criar o botão de scroll até o topo da página. Lembrando que esse botão só deve aparecer 
     a partir do momento que o scroll começou a ir para baixo. 
 
+    //Extra: fazerem o máximo de efeitos e eventos possível! 
 
 */
+
+$(document).ready(function () {
+  /********************
+
+     MENU DA ESQUERDA COM SCROLL ANIMADO 
+
+    ***********************/
+
+  $(".link1").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#h1").offset().top,
+      },
+      1000
+    );
+  });
+  $(".link2").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#h2").offset().top,
+      },
+      1000
+    );
+  });
+
+  $(".link3").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#h3").offset().top,
+      },
+      1000
+    );
+  });
+});
+/********************
+
+ SCROLL TOP NO BOTÃO TOPS 
+
+***********************/
+
+$(document).scroll(function () {
+  let topPosition = $(this).scrollTop();
+
+  if (topPosition > 50) {
+    $(".scrollTop").css("opacity", "1");
+  } else {
+    $(".scrollTop").css("opacity", "0");
+  }
+
+  $(".scrollTop").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
+});
